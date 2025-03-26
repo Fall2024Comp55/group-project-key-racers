2508ba310477f00003f8642d05311a0ce4429288
Son <s_nguyen91@u.pacific.edu>
2025-03-26 16:16:52 -0700

    Created GameScreenPane class to switch into game screen when play button is pressed.

--- src/GameScreenPane.java
+++ src/GameScreenPane.java
@@ -1,4 +1,20 @@
+import acm.graphics.GObject;
 
-public class GameScreenPane {
+public class GameScreenPane extends GraphicsPane {
+	public GameScreenPane(MainApplication mainScreen) {
+		this.mainScreen = mainScreen;
+	}
+	
+	@Override
+	public void showContent() {
+		
+	}
 
+	@Override
+	public void hideContent() {
+		for(GObject item : contents) {
+			mainScreen.remove(item);
+		}
+		contents.clear();
+	}
 }
