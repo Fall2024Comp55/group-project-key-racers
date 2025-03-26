4b8454f50872867cf4f427b6eff524da39c25d96
khaledomar <khaledomar@dhcp-10-15-130-229.wireless.pacific.edu>
2025-03-26 16:15:11 -0700

    Added window size, " WINDOW_WIDTH, AND WIDOW HRIGHT" and removed the add BackButton(), and add textButton()

--- src/DescriptionPane.java
+++ src/DescriptionPane.java
@@ -4,14 +4,21 @@ import java.awt.event.MouseEvent;
 import acm.graphics.*;
 
 public class DescriptionPane extends GraphicsPane{
+	
+	// window size
+		public static final int WINDOW_WIDTH = 800;
+		public static final int WINDOW_HEIGHT = 600;
+		
+		
+		
 	public DescriptionPane(MainApplication mainScreen) {
 		this.mainScreen = mainScreen;
 	}
 	
+	
 	@Override
 	public void showContent() {
-		addText();
-		addBackButton();
+		
 	}
 
 	@Override
@@ -22,24 +29,9 @@ public class DescriptionPane extends GraphicsPane{
 		contents.clear();
 	}
 	
-	private void addText() {
-		GLabel text = new GLabel("This is an example of a new screen with some description!", 100, 70);
-		text.setColor(Color.BLUE);
-		text.setFont("DialogInput-PLAIN-24");
-		text.setLocation((mainScreen.getWidth() - text.getWidth()) / 2, 70);
-		
-		contents.add(text);
-		mainScreen.add(text);
-	}
 	
-	private void addBackButton() {
-		GImage backButton = new GImage("back.jpg", 200, 400);
-		backButton.scale(0.3, 0.3);
-		backButton.setLocation((mainScreen.getWidth() - backButton.getWidth())/ 2, 400);
-		
-		contents.add(backButton);
-		mainScreen.add(backButton);
-	}
+	
+
 	
 	@Override
 	public void mouseClicked(MouseEvent e) {
