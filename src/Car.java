6425d38c69070f849119bcc6eebe9868c50c3ecf
khaledomar <khaledomar@dhcp-10-15-130-229.wireless.pacific.edu>
2025-03-27 16:20:22 -0700

    Created a Car class following the UML diagram.

---
+++ src/Car.java
@@ -0,0 +1,37 @@
+
+public class Car {
+	private int speed;
+	private int originalSpeed;
+	private int positionX;
+	private int leftBoundary;
+	private int rightBoundary;
+	
+	
+	public Car() {
+		
+	}
+	
+	
+	
+	public void increaseSpeed(int speed) {
+		
+	}
+	
+	
+	public void restSpeedForStart() {
+		
+	}
+	
+	public void moveLeft() {
+		
+		
+	}
+	
+	public void moveRight() {
+		
+	}
+	
+	public void checkBoundaries() {
+		
+	}
+}
