0d6798196a601e4220344073bb27890fecf349ec
Lisa <l_okamoto1@u.pacific.edu>
2025-03-28 16:06:49 -0700

    Added functions and a comment in RaceTimer

--- src/RaceTimer.java
+++ src/RaceTimer.java
@@ -1,4 +1,25 @@
 
 public class RaceTimer {
-
+	//This is the equivalent to 3 minutes in milliseconds
+	private int timeLeft = 180000;
+	private boolean isRunning = false;
+	
+	
+	public void startCountdown(){
+		
+	}
+	
+	public void getTimeLeft(){
+		
+		
+	}
+	
+	public void decreaseTime() {
+		
+	}
+	
+	public void stopCountdown(){
+		
+	}
+	
 }
