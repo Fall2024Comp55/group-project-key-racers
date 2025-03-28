f987fe1f359c69b38e855d8a361b7a3f0a04be10
Lisa <l_okamoto1@u.pacific.edu>
2025-03-28 16:13:35 -0700

    Added comments for methods in RaceTimer

--- src/RaceTimer.java
+++ src/RaceTimer.java
@@ -5,19 +5,23 @@ public class RaceTimer {
 	private boolean isRunning = false;
 	
 	
+	//Race timer starts running after the 3 second count down
 	public void startCountdown(){
-		
+	
 	}
 	
+	//For the car class to get time for the car to increase speed
 	public void getTimeLeft(){
 		
 		
 	}
 	
+	//Keeping track of the time decreasing in the timer
 	public void decreaseTime() {
 		
 	}
 	
+	//Timer stops when it reaches to 0 and a winner is determined
 	public void stopCountdown(){
 		
 	}
