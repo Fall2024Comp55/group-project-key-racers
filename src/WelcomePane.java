dc8dcfba53a32d5a0e17a1c1ec93a7e451d72214
Son <s_nguyen91@u.pacific.edu>
2025-03-28 15:58:45 -0700

    fixed a bug with accessing contents in WelcomePane.java

--- src/WelcomePane.java
+++ src/WelcomePane.java
@@ -64,7 +64,7 @@ public class WelcomePane extends GraphicsPane{
 		if (mainScreen.getElementAtLocation(e.getX(), e.getY()) == contents.get(1)) {
 			mainScreen.switchToDescriptionScreen();
 		}
-		if (mainScreen.getElementAtLocation(e.getX(), e.getY()) == contents.get(2)) {
+		else if (mainScreen.getElementAtLocation(e.getX(), e.getY()) == contents.get(2)) {
 			mainScreen.switchToGameScreen();
 		}
 	}
