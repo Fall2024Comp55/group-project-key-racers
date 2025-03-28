e87a5d787b6e6709823237e3fbed82e2513f273f
Son <s_nguyen91@u.pacific.edu>
2025-03-28 16:12:51 -0700

    Readjusted the road image to take up screen

--- src/GameScreenPane.java
+++ src/GameScreenPane.java
@@ -10,8 +10,8 @@ public class GameScreenPane extends GraphicsPane {
 	
 	public GameScreenPane(MainApplication mainScreen) {
 		this.mainScreen = mainScreen;
-		car1 = new Car("RedCar.png", 200, 500,200,500);
-		car2 = new Car("BlueCar.png", 500,500,200,500);
+		car1 = new Car("RedCar.png", 200, 500, 200, 500);
+		car2 = new Car("BlueCar.png", 500, 500, 200, 500);
 		
 	}
 	
@@ -38,8 +38,8 @@ public class GameScreenPane extends GraphicsPane {
 	// adds the backdrop of the road
 	private void addRoad(){
 		GImage roadImage = new GImage("Roads.png", 200, 100);
-		roadImage.scale(0.85, 0.75);
-		roadImage.setLocation((mainScreen.getWidth() - roadImage.getWidth())/ 2, 70);
+		roadImage.scale(0.85, 1);
+		roadImage.setLocation((mainScreen.getWidth() - roadImage.getWidth())/ 2, 0);
 		
 		contents.add(roadImage);
 		mainScreen.add(roadImage);
