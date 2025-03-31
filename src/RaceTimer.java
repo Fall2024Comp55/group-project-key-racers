5c66b9bbae633be128c7dfce56453f055bc91bf3
Son <s_nguyen91@u.pacific.edu>
2025-03-31 14:14:15 -0700

    Created 3 minute timer, finished all functions in RaceTimer class

--- src/RaceTimer.java
+++ src/RaceTimer.java
@@ -1,29 +1,49 @@
+import javax.swing.Timer;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class RaceTimer {
-	//This is the equivalent to 3 minutes in milliseconds
-	private int timeLeft = 180000;
-	private boolean isRunning = false;
+	//This is the equivalent to 3 minutes in seconds
+	private int timeLeft = 180;
+	private boolean timerRunning = false;
+	private Timer countdownTimer;
 	
 	
-	//Race timer starts running after the 3 second count down
+	//Race timer starts running after the 3 second count down; will add this later
 	public void startCountdown(){
+		if (timerRunning) {
+			return;
+		}
+		
+		timerRunning = true;
+		countdownTimer = new Timer(1000, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (timeLeft > 0) {
+                    timeLeft--;
+                } else {
+                    stopCountdown();
+                }
+            }
+        });
+
+        countdownTimer.start();
 	
 	}
 	
 	//For the car class to get time for the car to increase speed
-	public void getTimeLeft(){
-		
+	public int getTimeLeft(){
+		return timeLeft;
 		
 	}
 	
-	//Keeping track of the time decreasing in the timer
-	public void decreaseTime() {
-		
-	}
 	
 	//Timer stops when it reaches to 0 and a winner is determined
 	public void stopCountdown(){
-		
+		if (countdownTimer != null) {
+            countdownTimer.stop();
+        }
+        timerRunning = false;
 	}
 	
 }
