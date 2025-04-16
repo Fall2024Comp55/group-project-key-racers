1b8192ca581147e1223f1639cc345432a8344b72
Lisa <l_okamoto1@u.pacific.edu>
2025-04-16 15:51:51 -0700

    Polish code and add comments for methods in WinScreenPane class

--- src/WinScreenPane.java
+++ src/WinScreenPane.java
@@ -7,12 +7,13 @@ public class WinScreenPane extends GraphicsPane {
 	private SoundPlayer endMusic = new SoundPlayer();
 	private GameScreenPane gameScreenPane;
 	
-	
+	//Sets up the winning screen pane once the game ends as it goes over the screen to show winner
 	public WinScreenPane(MainApplication mainScreen, GameScreenPane gameScreenPane) {
 		this.mainScreen = mainScreen;
 		this.gameScreenPane = gameScreenPane;
 	}
 	
+	//Shows the content of the win screen pane
 	@Override
 	public void showContent() {
 		addReturnTitleButton();
@@ -20,6 +21,7 @@ public class WinScreenPane extends GraphicsPane {
 		playMusic();
 	}
 
+	//Hides the content of the win screen pane
 	@Override
 	public void hideContent() {
 		for(GObject item : contents) {
@@ -28,6 +30,7 @@ public class WinScreenPane extends GraphicsPane {
 		contents.clear();
 	}
 	
+	//Adds button for the player to return to the welcome pane/main menu
 	private void addReturnTitleButton() {
 		GImage titleButton = new GImage("ReturnTitleButton.png", 200, 200);
 		titleButton.scale(0.5, 0.5);
@@ -38,6 +41,8 @@ public class WinScreenPane extends GraphicsPane {
 
 	}
 	
+	//Display the winner of the game
+	//Note: Add a screen in case the players tied
 	private void addWinnerDisplay() {
 		if (gameScreenPane.getPlayerOneScore() > gameScreenPane.getPlayerTwoScore()) {
 			GImage blueWinner = new GImage("media/WinScreenBlue.png", 200, 100);
@@ -46,28 +51,21 @@ public class WinScreenPane extends GraphicsPane {
 			
 			contents.add(blueWinner);
 			mainScreen.add(blueWinner);
-		}
-		// add tie
-		else {
+		}else {
 			GImage redWinner = new GImage("media/WinScreenRed.png", 200, 100);
 			redWinner.setLocation((mainScreen.getWidth() - redWinner.getWidth())/ 2, 0);
 			
 			contents.add(redWinner);
 			mainScreen.add(redWinner);
 		}
-		/* if (score1 > score2) this would be for the score
-		 * GImage Winner = imageBlue...
-		 * else 
-		 * GImage Winner = imageRed...
-		 */
-		
 	}
 	
+	//Plays the background sound for the winner screen
 	private void playMusic() {
 		endMusic.playEndSound("media/Gameend.wav");
 	}
 	
-	
+	//Allow mousse to click on button to return to the welcome screen/main menu
 	@Override
 	public void mouseClicked(MouseEvent e) {
 		if (mainScreen.getElementAtLocation(e.getX(), e.getY()) == contents.get(1)) {
